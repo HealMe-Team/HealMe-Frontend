b673a726ea061dea1d0709d6e1a8373b43e06a94
DiegoAlonso04 <132526677+DiegoAlonso04@users.noreply.github.com>
2024-10-28 22:54:21 -0500

    feat(Footer):add Footer Component

---
+++ src/components/Footer/Footer.component.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <p>{{ $t('footer.copyright') }}</p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #000000;
+  color: #ffffff;
+  text-align: center;
+  padding: 10px;
+  z-index: 10;
+}
+</style>
